3a4a401624bd88c4cf497703ef6870d0ce5124e6
lululemotjuste <147537333+lululemotjuste@users.noreply.github.com>
2023-10-11 12:13:08 -0400

    Update index.svelte

--- src/routes/policy/franklin/index.svelte
+++ src/routes/policy/franklin/index.svelte
@@ -21,10 +21,67 @@
 
 let i = 0
 
-const next = () => {
-  i = (i + 1) % renders.length
-}
 </script>
+
+
+<style lang="scss">
+    .image-container {
+        position: relative;
+        width: 300px;
+        overflow: hidden;
+        cursor: pointer;
+    }
+
+    .image-container img {
+        width: 100%;
+        height: 100%;
+    }
+
+    .image-text {
+        position: absolute;
+        bottom: -100%; /* Start from below the image */
+        width: 100%;
+        background: rgba(0, 0, 0, 0.75);
+        color: #fff;
+        transition: bottom 0.3s ease-in-out; /* Animation */
+        padding: 10px;
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    .image-container:hover .image-text {
+        bottom: 0;
+        height: 100%;
+    }
+
+    a {
+        text-decoration: none;
+        flex-grow: 1;
+        max-width: 400px;
+    }
+</style>
+
+<svelte:head>
+    <title>UCP - Franklin</title>
+</svelte:head>
+
+<h1 class="text-primary">Reclaim Franklin for Housing</h1>
+<div class="d-flex p-4">
+    <div class="text-start p-2">
+        // <img class="mobile mb-2" src="/recode/Rosefoods.JPG" alt="Neighborhood example" style="width: 100%;"/>
+        // <p>The City of Portland is in the process of rewriting its zoning code. We applaud the ongoing work as well as the work done in Phase One of this ReCode process, which simplified our code, allowed the construction of accessory dwelling units, and took the first step towards transit-oriented development. However, from following the process, we are concerned the proposed changes for ReCode Phase Two will not take full advantage of the rare opportunity we have to improve our city. This is why we have put together our own proposal of code changes.</p>
+        // <p>The intention of these zoning code changes is to allow for more middle-density, mixed-use neighborhoods that are pleasant to live in and move around. These changes make housing more affordable, encourage equity, protect the environment, make the administration of our city services more resilient and efficient, and bring communities closer together. All of these outcomes are goals laid out in Portland’s Comprehensive Plan. The recommendations in this document will not stop people from building the types of buildings they build today, they will just give people more options of neighborhoods to build and live in. We are currently in a housing affordability crisis with an environmental crisis on the horizon; the least we can do is allow ourselves to build the kind of city that can rise to face these challenges.</p>
+    </div>
+    <div class="text-start p-2 desktop">
+        <img src="/recode/Rosefoods.JPG" alt="Neighborhood example" style="width: 100%;"/>
+    </div>
+</div>
+<h2 class="text-primary">Learn More</h2>
+<ul class="text-start">
+    <li><a href="https://thetownsman.org/a-biography-of-franklin-street/">A Biography of Franklin Street</a></li>
+    <li><a href="https://thetownsman.org/stops-and-starts-the-history-of-franklin-street-redesign/">Stops and Starts: The History of Franklin Street Redesign</a></li>
+</ul>
+
 
 <div class="d-flex flex-wrap" style="justify-content: center;">
 	{#each [renders.image[i]] as src (i)}
@@ -34,117 +91,6 @@ const next = () => {
 	  {#if i%2 == 0} // is even}
 	    <img transition:slide {src} alt="" />
 	  {/if}
+	i = (i + 1) % renders.length
 	{/each}
 </div>
-
-
-
-// 
-    
-<div class="hero">
-    <Carousel
-			autoplay 
-			autoplayDuration={5000}
-		>
-      </>
-			</>
-			</>
-			</>
-			</>
-    </Carousel>
-</div>
-
-
-
-
-  
-<script>
-	
-
-	let photo;
-	let newPhoto;
-	let firstLoad = false;
-	let crossfading = false;
-  
-  // what is this doing?? finding random colors?
-  async function fetchData() {
-		const rand = Math.floor(Math.random() * 5000);
-		
-    const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=${rand}&_limit=1`);
-    const data = await res.json();
-
-    if (res.ok) {
-      return data;
-    } else {
-      throw new Error(data);
-    }
-  }
-	const  sleep = (ms) => new Promise(r => setTimeout(r, ms));
-	async function handleFetch() {
-		try {
-			const f = await fetchData();
-			console.log(f);
-			if (!photo) { // init
-				photo = f[0];
-				newPhoto = f[0];
-				return;
-			}
-			newPhoto = f[0];
-		} catch(e) {
-			console.log(e);
-		}
-	}
-	async function onLoad(){
-		if (newPhoto.id === photo.id) {
-			firstLoad = true;
-			return;
-		}	
-		crossfading = true;
-		await sleep(1000);
-		photo = newPhoto;
-		crossfading = false;
-	}
-	
-	onMount(() => handleFetch());
-</script>
-
-<button on:click={handleFetch}>
-	Call fetch
-</button>
-
-    
-<div class="container" class:firstLoad >
-	{#if photo}
-		<figure class="orig">
-			<img src={photo.url} alt="" on:load={onLoad} >
-		</figure>
-		{#if newPhoto.id !== photo.id}
-		<figure class="new" class:crossfading >
-				<img src={newPhoto.url} alt="" on:load={onLoad}>
-			</figure>
-		{/if}
-	{/if}
-</div>
-
-    
-<style>
-	.container {
-		height: 10rem;
-	}
-	figure {
-		position: absolute;
-	}
-	.firstLoad figure.orig img {
-		opacity: 1;
-	}
-	figure.new {
-		z-index: 1;
-	}
-	figure.new.crossfading img {
-		opacity: 1;
-	}
-	img {
-		opacity: 0;
-		transition: all 1s ease;
-	}
-</style>
