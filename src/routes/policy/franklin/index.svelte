c4c5c0d6f3ed37ef280597c86625b736376ce942
lululemotjuste <147537333+lululemotjuste@users.noreply.github.com>
2023-10-12 00:33:15 -0400

    Update index.svelte

--- src/routes/policy/franklin/index.svelte
+++ src/routes/policy/franklin/index.svelte
@@ -1,25 +1,40 @@
 <script lang="ts">
   import { onMount } from 'svelte';	
   import { fade } from "svelte/transition";
-
-// declare array of render images
-
-  const renders = [{
-    image: '/FRANK-20230918-Photo_Housing.jpg',
-    desc: ''
-  },{
-    image: '/FRANK-20230918-Render_Housing_Labeled.jpg',
-    desc: ''
-  },{
-    image: '/FRANK-20230918-Photo_Park.jpg',
-    desc: ''
-  },{
-    image: '/FRANK-20230918-Render_Park_Labeled.jpg',
-    desc: ''
-  }]
+  import { Carousel } from "svelte-carousel";
 
 
-let i = 0
+ const stats = [{
+	number: '5.9 acres'
+	units: 'of city-owned land reclaimed'
+	color:
+	image:
+    }, {
+	number: '994 new affordable units'
+	units: 'under current zoning conditions'
+	color:
+	image:
+    }, {
+	number: '$15,700,000'
+	units: 'estimated sale value'
+	color:
+	image:
+    }, {
+	number: '$3,400,000'
+	units: 'estimated annual property tax value of affordable housing'
+	color:
+	image:
+    }, {
+	number: '17,000 SF'
+	units: 'Lincoln Park expansion'
+	color:
+	image:
+    }, {
+	number: '2.9 acres'
+	units: 'unbuilt potential green space'
+	color:
+	image:
+    }]
 
 </script>
 
@@ -65,32 +80,68 @@ let i = 0
     <title>UCP - Franklin</title>
 </svelte:head>
 
+
+
+        <p>A Vision of the Franklin Reclamation Authority</p>
+        <p>Presented in Collaboration with UCP</p>
+
+
+
 <h1 class="text-primary">Reclaim Franklin for Housing</h1>
 <div class="d-flex p-4">
+
+        <p>A team of assembled this plan to show the potential of the Franklin Street right-of-way to be redeveloped for housing in the heart of the Portland peninsula. This is not a specific plan or proposal, but a conceptual analysis of housing development opportunities in the Franklin Street corridor. </p>
+
+// The Vision
+<h2 class="text-primary">A Vision for Housing on Franklin</h2>
     <div class="text-start p-2">
-        // <img class="mobile mb-2" src="/recode/Rosefoods.JPG" alt="Neighborhood example" style="width: 100%;"/>
-        // <p>The City of Portland is in the process of rewriting its zoning code. We applaud the ongoing work as well as the work done in Phase One of this ReCode process, which simplified our code, allowed the construction of accessory dwelling units, and took the first step towards transit-oriented development. However, from following the process, we are concerned the proposed changes for ReCode Phase Two will not take full advantage of the rare opportunity we have to improve our city. This is why we have put together our own proposal of code changes.</p>
-        // <p>The intention of these zoning code changes is to allow for more middle-density, mixed-use neighborhoods that are pleasant to live in and move around. These changes make housing more affordable, encourage equity, protect the environment, make the administration of our city services more resilient and efficient, and bring communities closer together. All of these outcomes are goals laid out in Portland’s Comprehensive Plan. The recommendations in this document will not stop people from building the types of buildings they build today, they will just give people more options of neighborhoods to build and live in. We are currently in a housing affordability crisis with an environmental crisis on the horizon; the least we can do is allow ourselves to build the kind of city that can rise to face these challenges.</p>
+        <img class="mobile mb-2" src="/franklin/Render_Housing_Labeled.JPG" alt="" style="width: 100%;"/>
+        <img class="mobile mb-2" src="/franklin/Render_Park_Labeled.JPG" alt="" style="width: 100%;"/>
     </div>
     <div class="text-start p-2 desktop">
-        <img src="/recode/Rosefoods.JPG" alt="Neighborhood example" style="width: 100%;"/>
+        <img src="/franklin/Render_Housing_Labeled.JPG" alt="" style="width: 100%;"/>
+        <img src="/franklin/Render_Park_Labeled.JPG" alt="" style="width: 100%;"/>
     </div>
 </div>
+
+
+//The Numbers
+<h2 class="text-primary">The Numbers</h2>
+
+<div class="hero">
+	<Carousel
+	  autoplay
+	  autoplayDuration={5000}
+	  pauseOnFocus
+	>
+	  {#each stats as stat}
+	    <stat.number stat.units />
+	  {/each}
+	</Carousel>
+</div>
+
+//The Benefits
+<h2 class="text-primary">The Benefits</h2>
+
+
+
+// Learn More
 <h2 class="text-primary">Learn More</h2>
+  <div class="text-start p-2">
+	<p>To learn more about the history of Franklin Street and past attempts to redesign it, check out this series of deep-dive articles in The Portland Townsman. </p>
+  </div>
 <ul class="text-start">
     <li><a href="https://thetownsman.org/a-biography-of-franklin-street/">A Biography of Franklin Street</a></li>
     <li><a href="https://thetownsman.org/stops-and-starts-the-history-of-franklin-street-redesign/">Stops and Starts: The History of Franklin Street Redesign</a></li>
 </ul>
 
 
-<div class="d-flex flex-wrap" style="justify-content: center;">
-	{#each [renders.image[i]] as src (i)}
-	  {#if i%2 != 0} // is odd}
-	    <img transition:fade {src} alt="" />
-	  {/if}
-	  {#if i%2 == 0} // is even}
-	    <img transition:slide {src} alt="" />
-	  {/if}
-	i = (i + 1) % renders.length
-	{/each}
-</div>
+
+// Take Action
+<h2 class="text-primary">Take Action</h2>
+
+
+
+
+
+
